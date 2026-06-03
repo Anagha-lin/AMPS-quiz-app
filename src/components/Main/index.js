e9724ee06fecde8a97d8c58cdd5c6b14d738071c
Anagha-lin <linusanagha37@gmail.com>
2026-06-03 14:11:30 -0700

    image

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,7 +10,7 @@ import {
   Message,
 } from 'semantic-ui-react';
 
-import mindImg from '../../images/mind.svg';
+import mindImg from '../../images/mind.png';
 import { COUNTDOWN_TIME } from '../../constants';
 
 const Main = ({ startQuiz }) => {
